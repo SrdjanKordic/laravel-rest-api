8cf02d265664d576fc08e2177ac500ad4eda66cb
Srdjan Kordic <srdjank90@gmail.com>
2021-09-27 13:39:56 +0200

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): Response{
+        $credentials = $request->only('email','password');
+       
+        if(Auth::attempt($credentials)){
+            return response(Auth::user(),200);
+        }
+
+        abort(401);
+    }
+
+    public function logout(){
+        Auth::logout();
+        return response(null,200);
+    }
+}
