8536482d94d12de1a817d21ebe98a9210c79501e
Srdjan Kordic <srdjank90@gmail.com>
2021-10-05 10:26:00 +0200

    Added email for reset password

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -6,6 +6,9 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+//use Tymon\JWTAuth\Middleware\GetUserFromToken;
 
 class SendMail extends Mailable
 {
@@ -28,6 +31,7 @@ class SendMail extends Mailable
      */
     public function build()
     {
+        Log::info("Email message");
         return $this->markdown('Email.resetPassword')->with([
             'token' => $this->token
         ]);
--- resources/views/Email/resetPassword.blade.php
+++ resources/views/Email/resetPassword.blade.php
@@ -1,12 +1,9 @@
-@component('mail::message')
-# Introduction
-
-The body of your message.
-
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
-
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent
+<div id="email-wrapper" style="width: 600px;margin:0 auto;padding:30px;margin-top:80px;border:3px solid rgba(0,0,0,.075);border-radius:30px">
+    <h2 style="text-align: center;font-size:24px">You requested to reset your password</h2>  <br>
+    <p style="text-align: center">In order to reset your password please click on the next link</p>
+    <a style="margin-left:200px;text-align:center;padding: 6px 12px;font-size: 14px;border-radius: 6px;border: 0;color: #fff;background-color: #7367f0;border-color: #7367f0;text-decoration: none;" href="http://localhost:8080/reset-password?token={{ $token }}">Reset Password</a>
+    <br>
+    <br>
+    <p style="font-size: 14px;margin:0;border-bottom:1px solid rgba(0,0,0,.075);padding-bottom:25px;margin-bottom:3px">Best regards Srdjan Kordic</p> 
+    <p style="text-align: center;padding:0;margin:0;font-size:12px">All rights reserved © 2021 | twitter | google | github </p>
+</div>
